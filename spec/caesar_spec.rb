9afed58e14b718267d0d4a64394e336223be4812
Brandon Kounse <kounse@gmail.com>
2023-07-15 23:30:02 -0500

    Removed single initialization test and added tests for obfuscate method

--- spec/caesar_spec.rb
+++ spec/caesar_spec.rb
@@ -10,10 +10,6 @@ describe CaesarCipher do
       expect { CaesarCipher.new('abc') }.to raise_error(ArgumentError)
     end
 
-    it 'raises exception if message contains non-letters' do
-      expect { CaesarCipher.new(12, 1) }.to raise_exception 'message must be letters only!'
-    end
-
     it 'raises exception if shift contains non-numerics' do
       expect { CaesarCipher.new('abc', 'abc') }.to raise_exception 'shift must be an integer only!'
     end
@@ -23,6 +19,28 @@ describe CaesarCipher do
       expect(cipher).to be_an_instance_of(CaesarCipher)
       expect(cipher.message).to eql('abc')
       expect(cipher.shift).to eql(4)
+    end
+  end
+
+  context 'obfuscate' do
+    it '("A", 2) standard shift returns C' do
+      cipher = CaesarCipher.new('A', 2)
+      expect(cipher.obfuscate).to eql('C')
+    end
+
+    it '("DEF", -4) negative shift returns ZAB' do
+      cipher = CaesarCipher.new('DEF', -4)
+      expect(cipher.obfuscate).to eql('ZAB')
+    end
+
+    it '("aXeZBr", 10) mixed case returns kHoJLb' do
+      cipher = CaesarCipher.new('aXeZBr', 10)
+      expect(cipher.obfuscate).to eql('kHoJLb')
+    end
+
+    it '("Hello, world!", 4) space/digits returns Lipps, asvph!' do
+      cipher = CaesarCipher.new('Hello, world!', 4)
+      expect(cipher.obfuscate).to eql('Lipps, asvph!')
     end
   end
 end
