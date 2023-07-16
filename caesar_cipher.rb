85d818c720821b1055cf0144b5c709416539a6fe
Brandon Kounse <kounse@gmail.com>
2023-07-15 23:29:12 -0500

    Refactored ascii_shift method through TDD

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,18 +1,23 @@
 class CaesarCipher
   attr_reader :message, :shift
 
-  # Upper unicode is 65(A) to 90(Z)
-  # Lower unicode is 97(a) to 122(z)
+  UPPER_START = 65
+  UPPER_END = 90
+  LOWER_START = 97
+  LOWER_END = 122
 
   def initialize(message, shift)
+    raise ArgumentError, 'shift must be an integer only!' if /[A-Za-z]/.match?(shift.to_s)
+
     @message = message
     @shift = shift
   end
 
   def obfuscate
     new_message = []
-    @message.split(' ').each do |char|
-      new_message << ascii_shift(char.ord)
+    @message.split('').each do |char|
+      new_char = ascii_shift(char.ord)
+      new_message << new_char
     end
     @message = new_message.join
   end
@@ -20,13 +25,11 @@ class CaesarCipher
   private
 
   def ascii_shift(char)
-    if char >= 65 && char <= 90 # Uppercase Letters
-      char = (char + @shift) % 26
-      char += 65 if char < 65
+    if char >= UPPER_START && char <= UPPER_END # Uppercase Letters
+      char = (((char - UPPER_START) + @shift) % 26) + UPPER_START
 
     elsif char >= 97 && char <= 122 # Lowercase Letters
-      char = (char + @shift) % 26
-      char += 97 if char < 97
+      char = (((char - LOWER_START) + @shift) % 26) + LOWER_START
     end
     char.chr
   end
